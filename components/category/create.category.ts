02859288f95e5c8087b4d0597c866a60cb083f34
SHEETALTHAKUR <sheetalthakur@bitcot.com>
2024-11-20 00:40:26 +0530

    log error

--- components/category/create.category.ts
+++ components/category/create.category.ts
@@ -4,6 +4,8 @@ export const createCategory = async (req: any, res: any) => {
       const category = await prisma?.category.create({ data: { name } });
       res.status(201).json(category);
     } catch (error) {
+      console.log("------------", error);
+      
       res.status(400).json({ message: 'Error creating category', error });
     }
   };
